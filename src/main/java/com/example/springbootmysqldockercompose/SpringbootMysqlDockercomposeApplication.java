fa9fcf7ba1527cff08add771a4e222cf5f6da328
quanhy5@163.com <000888>
2020-01-08 21:48:38 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootmysqldockercompose/SpringbootMysqlDockercomposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqldockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlDockercomposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlDockercomposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmysqldockercompose/SpringbootMysqlDockercomposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqldockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlDockercomposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
